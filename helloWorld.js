997765592ba3d3221476eaf883d8b5a67854bfad
Amelia Firor <Amelia.Elyse.Firor@gmail.com>
2017-05-03 14:58:09 -0400

    the ral thing at last

--- helloWorld.js
+++ helloWorld.js
@@ -6,3 +6,19 @@ x.onkeyup=y.onkeyup=function(ev){
         //console.log('Enter was pressed for '+this.id+' at '+Date()+' and the value was '+this.value)
     //}
 }
+
+// playing with Plot.ly
+$.getJSON('https://data.cdc.gov/api/views/epev-k6ss/rows.json')
+ .then(function(dt){
+     var year=[], fert=[]
+     dt.data.forEach(function(r,i){
+         console.log(r[10],r[9])
+         year[i]=parseFloat(r[9])
+         fert[i]=parseFloat(r[10])
+     });
+     pt = Plotly.plot( lala, [{
+         x:year,
+         y: fert }]
+      )
+      4
+ })
