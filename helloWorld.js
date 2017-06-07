82b32b3906b6313f6f85a659d9d7ab44bd331fcb
Jonas Almeida <jalmeida@mathbiol.org>
2017-06-07 13:01:46 -0400

    broke away from master

--- helloWorld.js
+++ helloWorld.js
@@ -2,17 +2,18 @@ console.log('helloWold.js is loaded')
 
 x.onkeyup=y.onkeyup=function(ev){
     //if(ev.key=='Enter'){
-        z.value=parseFloat(x.value)+parseFloat(y.value)
+        z.textContent=parseFloat(x.value)+parseFloat(y.value)
         //console.log('Enter was pressed for '+this.id+' at '+Date()+' and the value was '+this.value)
     //}
 }
 
 // playing with Plot.ly
-$.getJSON('https://data.cdc.gov/api/views/epev-k6ss/rows.json')
- .then(function(dt){
+
+$.getJSON('https://data.cdc.gov/api/views/epev-k6ss/rows.json',function(dt){
+    console.log('this after loadind the data',dt)
      var year=[], fert=[]
      dt.data.forEach(function(r,i){
-         console.log(r[10],r[9])
+         //console.log(r[10],r[9])
          year[i]=parseFloat(r[9])
          fert[i]=parseFloat(r[10])
      });
@@ -22,3 +23,5 @@ $.getJSON('https://data.cdc.gov/api/views/epev-k6ss/rows.json')
       )
       4
  })
+
+ console.log('is this before of after?')
